a163e7d3fcba156625efffc6342edf72a90d6b85
Digvijay <digvijag@thoughtworks.com>
2014-01-09 14:07:28 +0530

    Word count Tested

---
+++ test/digvijag/unixtools/WcOperationsTest.java
@@ -0,0 +1,31 @@
+package digvijag.unixtools;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class WcOperationsTest {
+    @Test
+    public void testCountChars() throws Exception {
+        WcOperations operation = new WcOperations();
+        int expected = 5;
+        int actual = operation.countChars("abcde");
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testCountLines() throws Exception {
+        WcOperations operation = new WcOperations();
+        int expected = 2;
+        int actual = operation.countLines("abcde\r\ndw");
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testCountWords() throws Exception {
+        WcOperations operation = new WcOperations();
+        int expected = 3;
+        int actual = operation.countWords("ab cd e");
+        assertEquals(actual, expected);
+    }
+}
